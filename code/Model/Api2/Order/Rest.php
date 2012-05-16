fce90b021865449acb7cd75f8774b91534948ba4
jasonevans1 <jasonevans1@yahoo.com>
2012-05-16 06:45:30 -0500

    Inital commit of api model classes for testing purposes.

---
+++ code/Model/Api2/Order/Rest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Abstract API2 class for admin order
+ * 
+ * @category   AdminOrder
+ * @package    Je_AdminOrderAPI
+ * @author     Jason Evans(https://github.com/jasonevans1)
+ */
+abstract class Je_AdminOrderAPI_Model_Api2_Order_Rest extends Mage_Sales_Model_Api2_Order
+{
+	
+}
---
+++ code/Model/Api2/Order/Rest/Admin/V1.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * API2 class for admin orders
+ *
+ * @category   AdminOrder
+ * @package    Je_AdminOrderAPI
+ * @author     Jason Evans(https://github.com/jasonevans1)
+ */
+class Je_AdminOrderAPI_Model_Api2_Order_Rest_Admin_V1 extends Je_AdminOrderAPI_Model_Api2_Order_Rest {
+
+	/**
+	 * Create product
+	 *
+	 * @param array $data
+	 * @return string
+	 */
+	protected function _create(array $data) {
+		Mage::log('Create Order Api Method');
+		return $this->_getLocation($product);
+	}
+	
+}
